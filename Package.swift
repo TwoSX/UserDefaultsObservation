6a3466fda65a227f72182e2680de659770dd6393
TwoSX <twosxx@gmail.com>
2026-01-19 10:01:54 +0800

    feat: 升级 swift-syntax

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the Swift 6.0 compatible version of SwiftSyntax
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "602.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
